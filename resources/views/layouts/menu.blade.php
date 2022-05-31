12e94dcc55e0c73021de5da38d5a8e8cb999f859
stevane ntoungsi <stevanentoungsi99@gmail.com>
2022-05-31 12:20:05 +0100

    Ajout du panier au menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,23 +1,30 @@
 <header>
 
-    <div class="menu">
-        <a href="#home">Accuiel</a>
-        <div class="subnav">
-          <button class="subnavbtn">Categories <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
-            <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
-          </svg></button>
-          <div class="subnav-content">
-            <a href="#company">SmarthPhones</a>
-            <a href="#team">Televiseurs HD Full Option</a>
-            <a href="#careers">Ordinateurs et Accessoires</a>
-            <a href="#careers">Electromenagers</a>
-            <a href="#careers">Autres Appareils Multimedia</a>
+    <div class="menu d-flex justify-content-between">
+        <div>
+            <a href="#home">Accuiel</a>
+            <div class="subnav">
+              <button class="subnavbtn">Categories <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+              </svg></button>
+              <div class="subnav-content">
+                <a href="#company">SmarthPhones</a>
+                <a href="#team">Televiseurs HD Full Option</a>
+                <a href="#careers">Ordinateurs et Accessoires</a>
+                <a href="#careers">Electromenagers</a>
+                <a href="#careers">Autres Appareils Multimedia</a>
+              </div>
+            </div>
+
+
+            <a href="#contact">Contacts</a>
+            <a href="#contact">A Propos</a>
+          </div>
+          <div>
+              <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="49" height="18" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
+                <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+              </svg></a>
           </div>
         </div>
-
-
-        <a href="#contact">Contacts</a>
-        <a href="#contact">A Propos</a>
-      </div>
 
 </header>
